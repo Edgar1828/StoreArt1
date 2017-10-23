430b68533d6cbbac184a7c56b83c2aeffe557b72
Edgar1828 <ecg_1828@hotmail.com>
2017-10-22 22:44:43 -0500

    Add files via upload

---
+++ WebSite1/App_Code/Service.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Services;
+
+[WebService(Namespace = "http://localhost/WebService",Name = "WebServicePrueba", Description = "Servicio Web de prueba")]
+[WebServiceBinding(ConformsTo = WsiProfiles.BasicProfile1_1)]
+// Para permitir que se llame a este servicio web desde un script, usando ASP.NET AJAX, quite la marca de comentario de la línea siguiente. 
+// [System.Web.Script.Services.ScriptService]
+
+public class Service : System.Web.Services.WebService
+{
+    public Service () {
+
+        //Elimine la marca de comentario de la línea siguiente si utiliza los componentes diseñados 
+        //InitializeComponent(); 
+    }
+
+    [WebMethod]
+    public string HelloWorld() {
+        return "Hola a todos";
+    }
+
+    [WebMethod]
+    public string World()
+    {
+        return "Hola a todos";
+    }
+
+    [WebMethod]
+    public string StoreArt()
+    {
+        return "https://tiendastoreart.wixsite.com/artpr";
+    }
+
+}
